a763665744d48e006933052d0e8358de87b98d96
Zissis Vassos <zissis21@gmail.com>
2018-07-22 10:48:45 +0300

    Added the girl-char in the engine.js Resources.load

--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png'
     ]);
     Resources.onReady(init);
 
